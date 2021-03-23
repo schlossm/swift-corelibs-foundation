83b81fecea2b3dfbd905259110ee7ba208955b11
Michael Schloss <mschloss11@gmail.com>
2021-03-23 18:28:52 -0500

    Updated comments

--- Tests/Foundation/Tests/TestUndoManager.swift
+++ Tests/Foundation/Tests/TestUndoManager.swift
@@ -1,11 +1,10 @@
 // This source file is part of the Swift.org open source project
 //
-// Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
+// Copyright (c) 2014 - 2021 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
 class TestUndoManager : XCTestCase {
     static var allTests: [(String, (TestUndoManager) -> () throws -> Void)] {
